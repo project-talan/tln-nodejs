91c95d403b973e050af99f40685b52f0c98c1c41
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-06-02 10:37:05 +0300

    ajv: cache of validators, handling schema compilation errors

--- spec/utils/jsv.spec.js
+++ spec/utils/jsv.spec.js
@@ -17,17 +17,35 @@ describe("JSON validator", () => {
     number: 0
   };
   //
+  const schema2 = {
+    "title": "Schema2",
+    "description": "",
+    "type": "object",
+    "properties": {
+      "number": {
+        "type": "invalid_type",
+        "description": "Number field"
+      }
+    },
+    "required": ["number"]
+  };
+  //
   beforeAll(() => {
     jsv.compile('schema1', schema1);
   });
   afterAll(() => {
   });
 
-  it("Validate correct scema1", () => {
+  it("Validate correct schema1", () => {
     expect(true).toEqual(jsv.validate('schema1', schema1Data));
   });
 
-  it("Validate incorrect scema1", () => {
+  it("Validate incorrect schema1", () => {
     expect(false).toEqual(jsv.validate('schema1', {}));
+  });
+
+  it("Validate schema2", () => {
+    expect(false).toEqual(jsv.isCompiled('schema2'));
+    expect(false).toEqual(jsv.compile('schema2', schema2));
   });
 });
--- src/utils/jsv.js
+++ src/utils/jsv.js
@@ -5,13 +5,25 @@ module.exports = function(options) {
   this.cache = {};
   //
   this.compile = function(id, schema) {
-    this.cache[id] = ajv.compile(schema);
+    if (!this.isCompiled(id)) {
+      try {
+        this.cache[id] = ajv.compile(schema);
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    }
+    return (this.isCompiled(id));
   }
   this.validate = function(id, data) {
     return this.cache[id](data);
   }
   this.errors = function(id) {
     return this.cache[id].errors;
+  }
+  //
+  this.isCompiled = function(id) {
+    return (this.cache[id] != null);
   }
   //
   return this;
