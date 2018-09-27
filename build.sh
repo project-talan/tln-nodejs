48b0cffaa3477b6823f234344bbde491cfa9a659
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-09-27 19:28:46 +0300

    Removed .env.sh script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-. ./.env.sh
+export $(cat ./.env | grep -v ^# | xargs)
 npm i
--- docker.run.sh
+++ docker.run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
-. ./.env.sh
+export $(cat ./.env | grep -v ^# | xargs)
 docker run -d --rm \
+ --env-file ./.env \
  -p $COMPONENT_PARAM_PORT:$COMPONENT_PARAM_PORT \
  -p $COMPONENT_PARAM_PORTS:$COMPONENT_PARAM_PORTS \
   --name ${COMPONENT_ID} ${COMPONENT_ID}:${COMPONENT_VERSION}
