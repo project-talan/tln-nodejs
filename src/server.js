7a565c960146853cc38fc945e8d02efc685e95d6
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2019-05-01 16:35:20 +0000

    Server 4 unit tests

--- src/server.js
+++ src/server.js
@@ -1,5 +1,147 @@
 'use strict';
 
+class Server {
+  /*
+  *
+  *
+  */
+  constructor(logger) {
+    this.logger = logger;
+    this.express = require('express')
+    this.app = this.express();
+    this.http = require('http');
+    this.httpServer = null;
+    this.https = require('https');
+    this.httpsServer = null;
+    //
+    this.params = require('./utils/params').create();
+    this.jsv = require('./utils/jsv').create(this.logger, { allErrors:true, removeAdditional:'all' });
+    this.reply = require('./utils/reply').create();
+    //
+    this.dotenv = require('dotenv').config();
+    this.stats = require('simple-stats-server')();
+  }
+
+  /*
+  *
+  *
+  */
+  validateReqBody(schemaId) {
+    return (req, res, next) => {
+      if (!this.jsv.validate(schemaId, req.body)){
+        return res.status(400).json(this.reply.fail(this.jsv.errors(schemaId)));
+      }
+      next();
+    };
+  }
+
+  /*
+  *
+  *
+  */
+  init(env) {
+    this.logger.trace('Initialize server');
+    this.params.load({ 
+      key:        { env:'COMPONENT_ID',                   def: 'org.talan.nodejs',  delim: null },
+      version:    { env:'COMPONENT_VERSION',              def: '0.1.0',             delim: null },
+      host:       { env:'COMPONENT_PARAM_HOST',           def: 'localhost',         delim: null },
+      lstn:       { env:'COMPONENT_PARAM_LSTN',           def: '0.0.0.0',           delim: null },
+      port:       { env:'COMPONENT_PARAM_PORT',           def: 9081,                delim: null },
+      ports:      { env:'COMPONENT_PARAM_PORTS',          def: 9444,                delim: null },
+      certs:      { env:'COMPONENT_PARAM_SSL_CERTS',      def: null,                delim: ',' },
+      whitelist:  { env:'COMPONENT_PARAM_CORS_WHITELIST', def: '*',                 delim: ',' }
+    }, env);
+    // add json body parser
+    const bodyParser = require('body-parser');
+    this.app.use(bodyParser.json());
+    this.app.use((err, req, res, next) => {
+      if (err instanceof SyntaxError) {
+        return res.status(400).json(this.reply.fail(`input json syntax error: '${err.message}'`));
+      } else {
+        next(err);
+      }
+    });
+    // Status endpoint
+    this.app.use('/stats', this.stats);
+
+    // Healthcheck endpoint
+    // json validator for healthcheck POST request
+    this.jsv.compile('hcSchema',
+      { 
+        "title": "Healthcheck options schema",
+        "description": "",
+        "type": "object",
+        "properties": {
+          "timeout": {
+            "type": "number",
+            "description": "Status update timeout"
+          }
+        },
+        "required": ["timeout"]
+      }
+    );
+    this.app.route('/healthcheck')
+      .get( (req, res) => {
+        return res.json(this.reply.success({}));
+      })
+      .post(
+        this.validateReqBody('hcSchema'),
+        (req, res) => {
+          return res.json(this.reply.success({key:"value"}));
+        }
+      );
+
+    return true;
+  }
+
+  /*
+  *
+  *
+  */
+  run() {
+    this.httpServer = this.http.createServer(this.app).listen(
+      { host: this.params.lstn,
+        port: this.params.port
+      }, (err) => {
+        if (err) {
+          this.logger.fatal(err);
+        } else {
+          const host = this.httpServer.address().address;
+          const port = this.httpServer.address().port;
+          this.logger.info(`Server is listening http://${host}:${port}`);
+          this.params.dump(this.logger);
+        }
+    });
+
+
+    // this.httpServer = this.https.createServer(options, app).listen(443);
+    return this;
+  }
+
+  /*
+  *
+  *
+  */
+  stop() {
+    this.httpServer.close();
+    // this.httpsServer.close();
+  }
+}
+
+
+module.exports.run = (logger, env) => {
+  const server = new Server(logger);
+  if (server.init(env)) { 
+    return server.run();
+  }
+  return null;
+}
+
+module.exports.run4tests = () => {
+  return module.exports.run(require('./utils/logger').create(3), {host: 'localhost', lstn:'localhost', port:8080, ports:8443});
+}
+
+/*
 module.exports = function(){
   const fs = require('fs');
   const path = require('path');
@@ -21,7 +163,7 @@ module.exports = function(){
   // run server
   context.run();
 
-/*
+
   // Common modules ============================================================
   const helmet = require('helmet');
   const session = require('express-session'); 
@@ -100,7 +242,8 @@ module.exports = function(){
   // API v1 ====================================================================
   const apiV1 = require('./api/v1/impl')(express, app, jsv, reply, helpers);
 
-*/
+
   //
   return context;
 }
+*/
